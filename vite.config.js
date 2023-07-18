b8af7e71401c959a7d0dd2d584690843817f553b
Janith Madarasinghe <janithpm9991@gmail.com>
2023-07-18 13:27:22 +0530

    Revert "basename"
    This reverts commit ca86fcf549a482fb163a537058484272d87698ec.

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/NITC/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
+
+
