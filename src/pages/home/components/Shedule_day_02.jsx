00f233b4454fc8a2ae080f674e2032b579b2e7b5
Prasad Lakshan <kpdplakshan@gmail.com>
2023-10-03 06:18:19 +0530

    chg: add agenda schedule

--- src/pages/home/components/Shedule_day_02.jsx
+++ src/pages/home/components/Shedule_day_02.jsx
@@ -1,9 +1,6 @@
 function Shedule_day_02() {
   return (
-    <section
-      id="shedule_day_02"
-      className=""
-    >
+    <section id="shedule_day_02" className="">
       <div className="container">
         <div className="moblie-title">
           <h4>Day 01</h4>
@@ -17,30 +14,106 @@ function Shedule_day_02() {
             >
               <div className="schedule-tab-content col-12 clearfix">
                 <div className="tab-content" id="myTabContent">
-                  <div className="tab-pane fade show active" >
-
+                  <div className="tab-pane fade show active">
                     <div className="schedule-card">
                       <div className="schedule-slot-time">
-                        <span> 9.00 AM - 12.30 AM</span>
+                        <span> 9.00 AM - 09.30 AM</span>
                       </div>
                       <div className="schedule-card-content">
-                        <div className="schedule-card-header" >
-                          <h4>Conference - Common Sessions</h4>
+                        <div className="schedule-card-header">
+                          <h4>
+                            Common Session - “Second Wave of IT Transformation:
+                            From Digital to GenerativeAI”
+                          </h4>
                         </div>
                         <div className="schedule-card-body">
-                          <span>Keynote Presentations</span>
+                          <span> Mr. Nitendra Rajput,</span>
                           <br />
-                          <span>Panel Discussion and Q&A</span>
+                          <span>
+                            Senior Vice President, AI Products, Cyber and
+                            Intelligence Solutions (CAI), MasterCard
+                          </span>
+                          <br />
+                          <span>Q&A</span>
                         </div>
                       </div>
                     </div>
-
+                    <div className="schedule-card">
+                      <div className="schedule-slot-time">
+                        <span> 9.30 AM - 10.30 AM</span>
+                      </div>
+                      <div className="schedule-card-content">
+                        <div className="schedule-card-header">
+                          <h4>
+                            Panel Discussion - Tech industry in ChatGPT era
+                          </h4>
+                        </div>
+                        <div className="schedule-card-body">
+                          <span>
+                            {" "}
+                            Mr. Nitendra Rajput - Senior Vice President, AI
+                            Products, Cyber and Intelligence Solutions (CAI)
+                          </span>
+                          <br />
+                          <span>
+                            Mr. Varun Sakhuja - Director, Product Development -
+                            Innovation & Customer Solutions for Mastercard in
+                            Asia Pacific
+                          </span>
+                          <br />
+                          <span>
+                            Dr. Ruvan Weerasinghe -Senior Lecturer at the
+                            University of Colombo School of Computing (UCSC).
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="schedule-card">
+                      <div className="schedule-slot-time">
+                        <span> 10.30 AM - 11.00 AM</span>
+                      </div>
+                      <div className="schedule-card-content">
+                        <div className="schedule-card-header">
+                          <h4>Morning Tea</h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="schedule-card">
+                      <div className="schedule-slot-time">
+                        <span> 11.00 AM - 11.40 AM</span>
+                      </div>
+                      <div className="schedule-card-content">
+                        <div className="schedule-card-header">
+                          <h4>Conference - Common Session</h4>
+                        </div>
+                        <div className="schedule-card-body">
+                          <span>Mr. Anthony Wong</span>
+                          <br />
+                          <span>President -IFIP</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="schedule-card">
+                      <div className="schedule-slot-time">
+                        <span> 11.45 AM - 12.25 AM</span>
+                      </div>
+                      <div className="schedule-card-content">
+                        <div className="schedule-card-header">
+                          <h4>Conference - Common Session</h4>
+                        </div>
+                        <div className="schedule-card-body">
+                          <span>Mr. Ramindu Randeniya</span>
+                          <br />
+                          <span>Business and Corporate Leadership Coach</span>
+                        </div>
+                      </div>
+                    </div>
                     <div className="schedule-card">
                       <div className="schedule-slot-time">
                         <span>12.30 PM - 1.30 PM</span>
                       </div>
                       <div className="schedule-card-content">
-                        <div className="schedule-card-header" >
+                        <div className="schedule-card-header">
                           <h4>Lunch</h4>
                         </div>
                       </div>
@@ -51,7 +124,7 @@ function Shedule_day_02() {
                         <span> 1.30 PM - 5.00 PM</span>
                       </div>
                       <div className="schedule-card-content">
-                        <div className="schedule-card-header"  >
+                        <div className="schedule-card-header">
                           <h4>Conference - Breakout Sessions</h4>
                         </div>
                         <div className="schedule-card-body">
@@ -59,7 +132,6 @@ function Shedule_day_02() {
                         </div>
                       </div>
                     </div>
-
                   </div>
                 </div>
               </div>
