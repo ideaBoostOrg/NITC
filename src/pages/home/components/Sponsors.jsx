8203b263c9e5ac097a922e6e041fa1719ead9806
M.R.MAYUNIKA <RuwandiMayunika01@gmail.com>
2024-08-08 18:10:13 +0530

    logo update

--- src/pages/home/components/Sponsors.jsx
+++ src/pages/home/components/Sponsors.jsx
@@ -16,6 +16,20 @@ import TBA from "../../../assets/img/TBA.png";
 // import logo_7 from '../../../assets/img/sponsors/logo-7.png'
 // import logo_8 from '../../../assets/img/sponsors/logo-8.png'
 // import logo_9 from '../../../assets/img/sponsors/logo-9.png'
+import master from '../../../assets/img/sponsors/strategicPartners/master.png';
+import dell from '../../../assets/img/sponsors/goldSponsors/DELL.png';
+import fortinet from '../../../assets/img/sponsors/goldSponsors/Fortinet.png';
+import gennext from '../../../assets/img/sponsors/goldSponsors/gennext.png';
+import JIT from '../../../assets/img/sponsors/goldSponsors/JIT.png';
+import LEAFFoundation from '../../../assets/img/sponsors/goldSponsors/LEAFFoundation.png';
+import MIT from '../../../assets/img/sponsors/goldSponsors/MIT.png';
+import ITN from '../../../assets/img/sponsors/electronicMediaPartners/ITN.png';
+import Asset6 from '../../../assets/img/sponsors/diamondSponsors/Asset6.png';
+import DMS from '../../../assets/img/sponsors/diamondSponsors/DMS.png';
+import SafeProject from '../../../assets/img/sponsors/diamondSponsors/SafeProject.png';
+import Sanfer from '../../../assets/img/sponsors/diamondSponsors/Sanfer.png';
+import Orin from '../../../assets/img/sponsors/diamondSponsors/Orin.png';
+
 
 function Sponsors() {
   return (
@@ -53,6 +67,23 @@ function Sponsors() {
         >
           <div className="col-lg-12">
             <div className="sponsors-logo text-center">
+            <p
+                style={{
+                  fontWeight: "600",
+                  textAlign: "center",
+                  marginTop: "20px",
+                  marginBottom: "20px",
+                  fontSize: "1rem",
+                }}
+              >
+                Strategic partners
+              </p>
+              <span className="strategic-partner-logo-common">
+                <img
+                  src={master}
+                  alt=""
+                />
+              </span>
               <p
                 style={{
                   fontWeight: "600",
@@ -64,12 +95,26 @@ function Sponsors() {
               >
                 Diamond sponsor
               </p>
-              <span className="sponsor-logo-common">
+              <span className="diamond-sponsor-logo">
                 <img
-                  src={TBA}
+                  src={DMS}
                   alt=""
                 />
               </span>
+              <div className="col-lg-12 col-md-4 col-sm-4 text-center">
+                <span className="diamond-sponsor-logo-common">
+                  <img src={Asset6} alt="" />
+                </span>
+                <span className="diamond-sponsor-logo-common">
+                  <img src={SafeProject} alt="" />
+                </span>
+                <span className="diamond-sponsor-logo-common">
+                  <img src={Sanfer} alt="" />
+                </span>
+                <span className="diamond-sponsor-logo-common">
+                  <img src={Orin} alt="" />
+                </span>
+              </div>
               <p
                 style={{
                   fontWeight: "600",
@@ -99,33 +144,33 @@ function Sponsors() {
               </p>
               <div className="col-lg-12 col-md-4 col-sm-4 text-center">
                 <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                  <img src={dell} alt="" />
                 </span>
                 <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                  <img src={fortinet} alt="" />
                 </span>
                 <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                  <img src={gennext} alt="" />
                 </span>
                 <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                  <img src={JIT} alt="" />
                 </span>
               </div>
               {/* <br /> */}
 
               <div className="col-lg-12 col-md-4 col-sm-4 text-center">
                 <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                  <img src={LEAFFoundation} alt="" />
                 </span>
                 {/* <span className="gold-sponsor-logo">
                   <img src={logo_6} alt="" />
                 </span> */}
                 <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                  <img src={MIT} alt="" />
                 </span>
-                <span className="sponsor-logo-common">
+                {/* <span className="sponsor-logo-common">
                   <img src={TBA} alt="" />
-                </span>
+                </span> */}
               </div>
               <br />
 
@@ -189,8 +234,8 @@ function Sponsors() {
                 Electronic Media Partner
               </p>
               <div className="col-lg-12 col-md-4 col-sm-4 text-center">
-                <span className="sponsor-logo-common">
-                  <img src={TBA} alt="" />
+                <span className="electronic-media-partner-logo">
+                  <img src={ITN} alt="" />
                 </span>
               </div>
 
