156007799f6bce70a7d87d0baaa5ccb904f90117
M.R.MAYUNIKA <RuwandiMayunika01@gmail.com>
2025-06-27 11:53:31 +0530

    add responsive feature to awards section

--- src/pages/home/components/Awards.jsx
+++ src/pages/home/components/Awards.jsx
@@ -8,12 +8,13 @@ const Awards = () => {
       <div className="nt-awards-container">
         <div className="nt-awards-content-wrapper">
           <div className="nt-awards-content">
-            <h2 className="nt-section-title nt-title-light-bg">{awardsDetails.title}</h2>
-            <p className="nt-awards-subtitle">{awardsDetails.subtitle}</p>
-            <p className="nt-awards-description">
+            <h2 className="nt-section-title nt-title-light-bg nt-mobile-view">{awardsDetails.title}</h2>
+            <p className="nt-awards-subtitle nt-mobile-view">{awardsDetails.subtitle}</p>
+            <p className="nt-awards-description nt-mobile-view">
               {awardsDetails.description}
             </p>            
             <button className="nt-awards-btn">{awardsDetails.button}</button>
+            <img src={awardsDetails.logo} alt="CSSL Logo" className="nt-awards-logo-mobile" />
           </div>
         </div>
         <div className="nt-awards-image-wrapper">
