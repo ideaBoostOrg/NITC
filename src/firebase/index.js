5a79853552d40c473cc4392cfa4d05be7b6d7158
Prasad Lakshan <kpdplakshan@gmail.com>
2025-07-17 17:24:04 +0530

    Comment out Firebase configuration and initialization code

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,30 +1,30 @@
-/* eslint-disable no-unused-vars */
-// Import the functions you need from the SDKs you need
-import { getFirestore } from "@firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-console.log("Project IDs:", import.meta.env);
+// /* eslint-disable no-unused-vars */
+// // Import the functions you need from the SDKs you need
+// import { getFirestore } from "@firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// console.log("Project IDs:", import.meta.env);
 
-console.log("Project ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+// console.log("Project ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const firestore = getFirestore(app);
 
-export { analytics, app, firestore };
+// export { analytics, app, firestore };
 
